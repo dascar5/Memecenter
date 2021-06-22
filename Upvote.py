1b33a37b1b906345d06052f10e546a2c5e5a79d1
DanteV <44278130+dascar5@users.noreply.github.com>
2021-06-23 01:15:59 +0200

    Update Upvote.py
    made public

--- Upvote.py
+++ Upvote.py
@@ -3,18 +3,15 @@ from selenium import webdriver
 browser = webdriver.Chrome()
 
 #GLOBAL VARIABLES
-page = 'https://www.memecenter.com/fun/3377699729004745/the-fbi-is-coming-for-me/comments'
+page = 'url of post where the comments are (make sure the url ends with /comments)'
 upvoteCSS = '#comments > div:nth-child(3) > div > table > tbody > tr > td:nth-child(2) > div.comment-buttons > div.upvote.act_comvote.login'
 loginNotifCSS = '#fdc_ntfnotf > div.notification-icon.act_dropdownact'
 usernameFieldCSS = '#l_username'
 passwordFieldCSS = '#l_password'
 loginCSS = '#login_form > div > div.npc-bottom > input'
-password = 'asdasd'
+password = 'your password, make sure your password is same for all accounts'
 logout = 'https://www.memecenter.com/auth/logout'
-accounts = ['xxx1','xxx2','xxx3','xxx4','xxx5','xxx6','xxx7','xxx8','zzz1','zzz2','zzz3','zzz4','zzz5','zzz6',
-            'zzz8','ccc1','ccc2','ccc3','ccc4','ccc5','ccc6','ccc7','ccc8','vvv1','vvv2','vvv3','vvv4','vvv5','vvv6',
-            'vvv7','vvv8','bbb1','bbb2','bbb3','bbb4','bbb5','bbb6','bbb7','bbb8','nnn1','nnn2','nnn3','nnn4','nnn5',
-            'nnn6','nnn7','nnn8','mmm1','mmm2','mmm3','mmm4','mmm5','mmm6','mmm7','mmm8',]
+accounts = ['all usernames of your many accounts','keep them separate like this']
 
 #script start
 browser.get(page)
